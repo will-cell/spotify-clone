e22726ea256cfbd569daf76b420304f2d855b234
will-cell <williamh.henri@yahoo.fr>
2021-01-10 13:36:28 +0100

    ok

---
+++ src/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SidebarOption.css'
+
+function SidebarOption( { title, Icon } ) {
+    return (
+        <div className="sidebarOption">
+            {title}
+        </div>
+    )
+}
+
+export default SidebarOption
